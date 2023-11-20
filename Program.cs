c771c1ac913eb746b7973562c8f81a984be88923
Esho <esho.goria2@gmail.com>
2023-11-20 20:58:16 +0100

    Fixme comments

--- Program.cs
+++ Program.cs
@@ -3,36 +3,48 @@
     internal class Program
     {
         static List<SweEngGloss> dictionary;
+
         class SweEngGloss
         {
             public string word_swe, word_eng;
+
+            // Constructor for initializing with provided Swedish and English words
             public SweEngGloss(string word_swe, string word_eng)
             {
-                this.word_swe = word_swe; this.word_eng = word_eng;
+                this.word_swe = word_swe;
+                this.word_eng = word_eng;
             }
+
+            // Constructor for initializing from a line in the file
             public SweEngGloss(string line)
             {
+                // FIXME: Potential issue if the line does not contain '|' or if words[1] doesn't exist
                 string[] words = line.Split('|');
-                this.word_swe = words[0]; this.word_eng = words[1];
+                this.word_swe = words[0];
+                this.word_eng = words[1];
             }
         }
+
         static void Main(string[] args)
         {
             string defaultFile = "..\\..\\..\\dict\\sweeng.lis";
             Console.WriteLine("Welcome to the dictionary app!");
+
             do
             {
                 Console.Write("> ");
                 string[] argument = Console.ReadLine().Split();
                 string command = argument[0];
+
                 if (command == "quit")
                 {
                     Console.WriteLine("Goodbye!");
                 }
                 else if (command == "load")
                 {
-                    if(argument.Length == 2)
+                    if (argument.Length == 2)
                     {
+                        // FIXME: No error handling if the specified file does not exist
                         using (StreamReader sr = new StreamReader(argument[1]))
                         {
                             dictionary = new List<SweEngGloss>(); // Empty it!
@@ -45,8 +57,9 @@
                             }
                         }
                     }
-                    else if(argument.Length == 1)
+                    else if (argument.Length == 1)
                     {
+                        // FIXME: No error handling if the default file does not exist
                         using (StreamReader sr = new StreamReader(defaultFile))
                         {
                             dictionary = new List<SweEngGloss>(); // Empty it!
@@ -62,7 +75,7 @@
                 }
                 else if (command == "list")
                 {
-                    foreach(SweEngGloss gloss in dictionary)
+                    foreach (SweEngGloss gloss in dictionary)
                     {
                         Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}");
                     }
@@ -71,9 +84,10 @@
                 {
                     if (argument.Length == 3)
                     {
+                        // FIXME: No input validation, assumes valid input
                         dictionary.Add(new SweEngGloss(argument[1], argument[2]));
                     }
-                    else if(argument.Length == 1)
+                    else if (argument.Length == 1)
                     {
                         Console.WriteLine("Write word in Swedish: ");
                         string s = Console.ReadLine();
@@ -87,7 +101,10 @@
                     if (argument.Length == 3)
                     {
                         int index = -1;
-                        for (int i = 0; i < dictionary.Count; i++) {
+
+                        // FIXME: No error handling if the specified words are not found
+                        for (int i = 0; i < dictionary.Count; i++)
+                        {
                             SweEngGloss gloss = dictionary[i];
                             if (gloss.word_swe == argument[1] && gloss.word_eng == argument[2])
                                 index = i;
@@ -101,6 +118,8 @@
                         Console.Write("Write word in English: ");
                         string e = Console.ReadLine();
                         int index = -1;
+
+                        // FIXME: No error handling if the specified words are not found
                         for (int i = 0; i < dictionary.Count; i++)
                         {
                             SweEngGloss gloss = dictionary[i];
@@ -114,8 +133,9 @@
                 {
                     if (argument.Length == 2)
                     {
-                        foreach(SweEngGloss gloss in dictionary)
+                        foreach (SweEngGloss gloss in dictionary)
                         {
+                            // FIXME: No handling for case sensitivity or partial matches
                             if (gloss.word_swe == argument[1])
                                 Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
                             if (gloss.word_eng == argument[1])
@@ -128,6 +148,7 @@
                         string s = Console.ReadLine();
                         foreach (SweEngGloss gloss in dictionary)
                         {
+                            // FIXME: No handling for case sensitivity or partial matches
                             if (gloss.word_swe == s)
                                 Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
                             if (gloss.word_eng == s)
@@ -139,8 +160,7 @@
                 {
                     Console.WriteLine($"Unknown command: '{command}'");
                 }
-            }
-            while (true);
+            } while (true);
         }
     }
 }
