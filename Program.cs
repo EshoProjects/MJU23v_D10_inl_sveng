e2cd89a368bcb46b3690bb5c61afad4e811b6825
Esho <esho.goria2@gmail.com>
2023-12-06 21:04:54 +0100

    Separata metoder

--- Program.cs
+++ Program.cs
@@ -12,15 +12,19 @@ namespace MJU23v_D10_inl_sveng
         {
             public string word_swe, word_eng;
 
-            // Constructor for initializing with provided Swedish and English words
-            public SweEngGloss(string word_swe, string word_eng)
+            public SweEngGloss(string line)
             {
-                this.word_swe = word_swe;
-                this.word_eng = word_eng;
             }
 
+            // Constructors and other members...
+
             // Constructor for initializing from a line in the file
-            public SweEngGloss(string line)
+            public SweEngGloss(string line, string? ea)
+            {
+                ParseLine(line);
+            }
+
+            private void ParseLine(string line)
             {
                 // FIXME: Potential issue if the line does not contain '|' or if words[1] doesn't exist
                 string[] words = line.Split('|');
@@ -48,145 +52,23 @@ namespace MJU23v_D10_inl_sveng
                     }
                     else if (command == "load")
                     {
-                        if (argument.Length == 2)
-                        {
-                            try
-                            {
-                                using (StreamReader sr = new StreamReader(argument[1]))
-                                {
-                                    dictionary = new List<SweEngGloss>(); // Empty it!
-                                    string line = sr.ReadLine();
-                                    while (line != null)
-                                    {
-                                        try
-                                        {
-                                            SweEngGloss gloss = new SweEngGloss(line);
-                                            dictionary.Add(gloss);
-                                        }
-                                        catch (Exception ex)
-                                        {
-                                            Console.WriteLine($"Error reading line from file: {ex.Message}");
-                                        }
-                                        line = sr.ReadLine();
-                                    }
-                                }
-                            }
-                            catch (FileNotFoundException)
-                            {
-                                Console.WriteLine($"File not found: {argument[1]}");
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine($"Error loading file: {ex.Message}");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid 'load' command syntax");
-                        }
+                        LoadDictionary(argument);
                     }
                     else if (command == "list")
                     {
-                        foreach (SweEngGloss gloss in dictionary)
-                        {
-                            Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}");
-                        }
+                        ListGlossary();
                     }
                     else if (command == "new")
                     {
-                        if (argument.Length == 3)
-                        {
-                            try
-                            {
-                                dictionary.Add(new SweEngGloss(argument[1], argument[2]));
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine($"Error adding new glossary entry: {ex.Message}");
-                            }
-                        }
-                        else if (argument.Length == 1)
-                        {
-                            try
-                            {
-                                Console.WriteLine("Write word in Swedish: ");
-                                string sa = Console.ReadLine();
-                                Console.Write("Write word in English: ");
-                                string ea = Console.ReadLine();
-                                dictionary.Add(new SweEngGloss(sa, ea));
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine($"Error adding new glossary entry: {ex.Message}");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid 'new' command syntax");
-                        }
+                        AddNewEntry(argument);
                     }
                     else if (command == "delete")
                     {
-                        if (argument.Length == 1)
-                        {
-                            try
-                            {
-                                Console.WriteLine("Write word in Swedish: ");
-                                string sa = Console.ReadLine();
-                                Console.Write("Write word in English: ");
-                                string ea = Console.ReadLine();
-                                int index = -1;
-
-                                for (int i = 0; i < dictionary.Count; i++)
-                                {
-                                    SweEngGloss gloss = dictionary[i];
-                                    if (gloss.word_swe == sa && gloss.word_eng == ea)
-                                        index = i;
-                                }
-                                dictionary.RemoveAt(index);
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine($"Error deleting glossary entry: {ex.Message}");
-                            }
-                        }
+                        DeleteEntry(argument);
                     }
                     else if (command == "translate")
                     {
-                        if (argument.Length == 1)
-                        {
-                            try
-                            {
-                                Console.WriteLine("Write word to be translated: ");
-                                string sa = Console.ReadLine().ToLower();  // Convert to lowercase for case-insensitive comparison
-
-                                bool translationFound = false;
-
-                                foreach (SweEngGloss gloss in dictionary)
-                                {
-                                    if (gloss.word_swe.Equals(sa, StringComparison.OrdinalIgnoreCase))
-                                    {
-                                        Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                                        translationFound = true;
-                                    }
-
-                                    if (gloss.word_eng.Equals(sa, StringComparison.OrdinalIgnoreCase))
-                                    {
-                                        Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
-                                        translationFound = true;
-                                    }
-                                }
-
-                                if (!translationFound)
-                                {
-                                    Console.WriteLine("Translation not found.");
-                                }
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine($"Error translating word: {ex.Message}");
-                            }
-                        }
+                        TranslateWord(argument);
                     }
                     else
                     {
@@ -198,6 +80,153 @@ namespace MJU23v_D10_inl_sveng
                     Console.WriteLine($"An unexpected error occurred: {ex.Message}");
                 }
             } while (true);
+        }
+
+        private static void LoadDictionary(string[] argument)
+        {
+            if (argument.Length == 2)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(argument[1]))
+                    {
+                        dictionary = new List<SweEngGloss>(); // Empty it!
+                        string line = sr.ReadLine();
+                        while (line != null)
+                        {
+                            try
+                            {
+                                SweEngGloss gloss = new SweEngGloss(line);
+                                dictionary.Add(gloss);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"Error reading line from file: {ex.Message}");
+                            }
+                            line = sr.ReadLine();
+                        }
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine($"File not found: {argument[1]}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error loading file: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid 'load' command syntax");
+            }
+        }
+
+        private static void ListGlossary()
+        {
+            foreach (SweEngGloss gloss in dictionary)
+            {
+                Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}");
+            }
+        }
+
+        private static void AddNewEntry(string[] argument)
+        {
+            if (argument.Length == 3)
+            {
+                try
+                {
+                    dictionary.Add(new SweEngGloss(argument[1], argument[2]));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error adding new glossary entry: {ex.Message}");
+                }
+            }
+            else if (argument.Length == 1)
+            {
+                try
+                {
+                    Console.WriteLine("Write word in Swedish: ");
+                    string sa = Console.ReadLine();
+                    Console.Write("Write word in English: ");
+                    string ea = Console.ReadLine();
+                    dictionary.Add(new SweEngGloss(sa, ea));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error adding new glossary entry: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid 'new' command syntax");
+            }
+        }
+
+        private static void DeleteEntry(string[] argument)
+        {
+            if (argument.Length == 1)
+            {
+                try
+                {
+                    Console.WriteLine("Write word in Swedish: ");
+                    string sa = Console.ReadLine();
+                    Console.Write("Write word in English: ");
+                    string ea = Console.ReadLine();
+                    int index = -1;
+
+                    for (int i = 0; i < dictionary.Count; i++)
+                    {
+                        SweEngGloss gloss = dictionary[i];
+                        if (gloss.word_swe == sa && gloss.word_eng == ea)
+                            index = i;
+                    }
+                    dictionary.RemoveAt(index);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error deleting glossary entry: {ex.Message}");
+                }
+            }
+        }
+
+        private static void TranslateWord(string[] argument)
+        {
+            if (argument.Length == 1)
+            {
+                try
+                {
+                    Console.WriteLine("Write word to be translated: ");
+                    string sa = Console.ReadLine().ToLower();  // Convert to lowercase for case-insensitive comparison
+
+                    bool translationFound = false;
+
+                    foreach (SweEngGloss gloss in dictionary)
+                    {
+                        if (gloss.word_swe.Equals(sa, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
+                            translationFound = true;
+                        }
+
+                        if (gloss.word_eng.Equals(sa, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
+                            translationFound = true;
+                        }
+                    }
+
+                    if (!translationFound)
+                    {
+                        Console.WriteLine("Translation not found.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error translating word: {ex.Message}");
+                }
+            }
         }
     }
 }
